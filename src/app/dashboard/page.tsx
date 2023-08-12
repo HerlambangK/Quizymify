45bf3213b9dec2c84a518610f835f9b48d536e65
herlambang k <herlambangk25@gmail.com>
2023-08-12 22:16:55 +0700

    add:Quiz card and historycard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,31 @@
+import HistoryCard from '@/components/dashboard/HistoryCard';
+import QuizMeCard from '@/components/dashboard/QuizMeCard';
+import { getAuthSession } from '@/lib/nextauth'
+import { redirect } from 'next/navigation';
 import React from 'react'
 
 type Props = {}
 
-const Dashboard = (props: Props) => {
+export const metadata = {
+    title: "Dashboard | Quizmify"
+}
+
+const Dashboard = async (props: Props) => {
+    const session = await getAuthSession();
+    if (!session?.user) {
+        return redirect('/')
+    }
     return (
-        <div>Dashboard</div>
+        <main className='p-8 mx-auto max-1-7xl'>
+            <div className="flex item-center">
+                <h2 className='mr-2 text-3xl font-bold tracking-tight'>Dashboard</h2>
+            </div>
+            <div className="grid gap-4 mt-4 md:grid-cols-2">
+                <QuizMeCard />
+                <HistoryCard />
+            </div>
+            <div className="grid gap-4 mt-4 md:grild-cols-2 lg:grid-cols-7"></div>
+        </main>
     )
 }
 
