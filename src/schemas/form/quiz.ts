10f929fad4232191ba8ac44117681341ab3c110d
herlambang k <herlambangk25@gmail.com>
2023-08-15 00:26:44 +0700

    add: endpoint question api

--- src/schemas/form/quiz.ts
+++ src/schemas/form/quiz.ts
@@ -6,7 +6,10 @@ export const quizCreationSchema = z.object({
         .min(4, {
             message: "Topic must be at leact 4 character long",
         })
-        .max(50),
+        .max(50, {
+            message: "Topic must be at most 50 characters long",
+        }),
     type: z.enum(["mcq", "open_ended"]),
     amount: z.number().min(1).max(10),
 });
+
