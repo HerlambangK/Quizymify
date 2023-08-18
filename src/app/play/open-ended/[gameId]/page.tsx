5e9c203339f2281da38e52056196af7c7f948cd6
herlambang k <herlambangk25@gmail.com>
2023-08-18 10:38:15 +0700

    feat: ui play mcq

---
+++ src/app/play/open-ended/[gameId]/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+function OpenEndedPage({ }: Props) {
+    return (
+        <div>OpenEndedPage
+
+        </div>
+    )
+}
+
+export default OpenEndedPage
